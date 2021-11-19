0e1ff721e8fd87fb8437bf8fb6648aba164bf5e1
CPunisher <1343316114@qq.com>
2021-11-20 00:51:30 +0800

    fix: variable pointer type

--- src/com/cpunisher/hayasai/ir/value/Block.java
+++ src/com/cpunisher/hayasai/ir/value/Block.java
@@ -123,7 +123,7 @@ public final class Block extends Value implements IVariableTable<Register, Liter
     }
 
     public Register putVar(Ident ident, Type type) {
-        Register register = this.functionDef.alloc(type);
+        Register register = this.functionDef.alloc(type.getPointer());
         this.localVars.putVar(ident, register);
         this.addSubToFront(new AllocaStatement(register, register.getType()));
         return register;
